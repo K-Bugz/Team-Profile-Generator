1d1d22f97ecd1fca6a215000a62587d8f5a19864
kbugz <kbugusky@gmail.com>
2022-02-28 12:17:00 -0600

    proper npm installs, added require for jest

---
+++ index.js
@@ -0,0 +1,11 @@
+// Local imported js classes
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRender");
+// Npm classes
+const inquirer = require("inquirer");
+const fs = require("fs");
+const jest = require("jest");
+
+const teamMember = []; // An array can be const
